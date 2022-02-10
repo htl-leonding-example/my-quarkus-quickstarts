40abe92100fb686cb32fabaa029e9fa225e33dea
stuetz <t.stuetz@htl-leonding.ac.at>
2022-02-10 23:58:32 +0100

    add try-with-resource for Files.lines(...)

--- file-read/src/main/java/at/htl/readfile/InitBean.java
+++ file-read/src/main/java/at/htl/readfile/InitBean.java
@@ -2,9 +2,11 @@ package at.htl.readfile;
 
 import io.quarkus.runtime.StartupEvent;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,37 +14,42 @@ import java.nio.file.Path;
 @ApplicationScoped
 public class InitBean {
 
+    @Inject
+    Logger log;
+
     @ConfigProperty(name = "file.name.csv")
     String fileName;
 
     void init(@Observes StartupEvent event) {
 
-        try {
-            // Einlesen eines einzelnen Wertes aus der Datei
-            String location = Files.lines(Path.of(fileName))
+        // Einlesen eines einzelnen Wertes aus der Datei
+        try (var s = Files.lines(Path.of(fileName))) {
+            var location = s
                     .skip(4)
                     .findFirst()
                     .map(line -> {
                         var elements = line.split(":");
-                        System.out.println(elements[1]);
+                        log.info(elements[1]);
                         return elements[1].trim();
                     })
                     .get();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
 
-            // Einlesen aller Temperaturen und transponieren für Ausgabe von yyyy-mm/temperature
-            Files.lines(Path.of(fileName))
-                    .skip(14)
+        // Einlesen aller Temperaturen und transponieren für Ausgabe von yyyy-mm/temperature
+        try (var s = Files.lines(Path.of(fileName))) {
+            s.skip(14)
                     .forEach(line -> {
                         var elements = line.split(";");
                         for (int i = 1; i < 13; i++) {
                             if (!elements[i].equals("999999")) {
-                                System.out.printf("%s-%d/%s\n", elements[0], i, elements[i]);
+                                log.infof("%s-%d/%s", elements[0], i, elements[i]);
                             }
                         }
                     });
-
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            log.error(e.getMessage());
         }
     }
 }
